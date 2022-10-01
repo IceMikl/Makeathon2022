e69f7ebd040047e679670330f0bc12dfbd98be0c
IceMikl <icemikl7@gmail.com>
2022-10-02 01:50:14 +0200

    Add prediction calculation to the backend server.

--- code/src/server/server.py
+++ code/src/server/server.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, request, Response, jsonify
+from predict import make_prediction
 
 app = Flask(__name__)
 
@@ -10,20 +11,22 @@ def hello():
 
 @app.route('/check_image', methods=['POST'])
 def check_image():
+    melanoma_image_path="melanoma_image_tmp.jpg"
     melanoma_image = request.files.get('melanoma_image', '')
-    print(melanoma_image)
-    __check_melanoma_image(melanoma_image=melanoma_image)
+    melanoma_image.save(melanoma_image_path)
+    results = __check_melanoma_image(melanoma_image_path=melanoma_image_path)
 
     data = {
         'melanoma_detected': 'false',
-        "processing_time": 0
+        "processing_time": 0,
+        "results": str(results)
     }
     return jsonify(data), 200
 
 
-def __check_melanoma_image(melanoma_image):
-    pass
-
+def __check_melanoma_image(melanoma_image_path):
+    results = make_prediction(melanoma_image_path)
+    return results
 
 if __name__ == '__main__':
     app.run(port=5566)
--- code/test/test_request.py
+++ code/test/test_request.py
@@ -3,13 +3,18 @@ import requests
 import json
 
 
-#url = 'http://localhost:5566/check_image'
-url_check_image = 'https://melanome-detection-server.azurewebsites.net/check_image'
-files = {'melanoma_image': open('JPG_Test.jpg', 'rb')}
-response_check_image = requests.post(url=url_check_image, files=files)
-print(json.loads(response_check_image.content))
+#url = 'http://localhost:5566/'
 
-url_test = 'https://melanome-detection-server.azurewebsites.net/test'
+url = 'https://melanome-detection-server.azurewebsites.net/'
+
+url_test = url + 'test'
 response_test = requests.get(url=url_test)
 print(response_test.content)
+
+
+url_check_image = url + 'check_image'
+files = {'melanoma_image': open('JPG_Test.jpg', 'rb')}
+response_check_image = requests.post(url=url_check_image, files=files)
+print(response_check_image.content)
+
 
